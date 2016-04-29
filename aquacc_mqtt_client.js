7de7edf8a6271daa925f0cc3ff534d00fc9c47c8
David Cluytens <david.cluytens@softathome.com>
2016-04-29 16:23:39 +0200

    cleanup

--- aquacc_mqtt_client.js
+++ aquacc_mqtt_client.js
@@ -1,15 +1,16 @@
 var mqtt    = require('mqtt');
 var client  = mqtt.connect('mqtt://localhost');
-
 var device  = "arduino01";
-var sensor  = Math.floor(((Math.random() * 100) % 2) + 1);
-var data    = (((Math.random() * 100) % 10) + 21).toFixed(2);
-sensor = 6;
-console.log(sensor); 
-console.log(data); 
+
 client.on('connect', function () {
-  var topic = "Measurements/Devices/" + device + "/Sensors/" + sensor;
-  client.publish(topic, data.toString());
-  client.end();
-});
- 
+        [1, 2, 3, 4, 5, 6, 7, 8, 9].forEach(function(sensor) {
+            var data    = (((Math.random() * 100) % 10) + 21).toFixed(2);
+            var topic = "Measurements/Devices/" + device + "/Sensors/" + sensor;
+            console.log(sensor); 
+            console.log(data); 
+
+            client.publish(topic, data.toString());
+            });
+        client.end();
+        });
+
